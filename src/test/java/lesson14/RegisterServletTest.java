b940c4092a01b25f8e798a461889c5743803dd45
Eric Leow <ericleowyuquan@gmail.com>
2025-07-23 18:12:14 +0800

    (feat) Add preliminary test cases

---
+++ src/test/java/lesson14/RegisterServletTest.java
@@ -0,0 +1,15 @@
+package lesson14;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class RegisterServletTest {
+
+	@Test
+	void firstTest() {
+		assertEquals(5, 5);
+	}
+}
