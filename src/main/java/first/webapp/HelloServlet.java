be330b45674c56493bdb13cd68df13363c6a4111
Eric Leow <ericleowyuquan@gmail.com>
2026-07-28 08:37:12 +0800

    test

--- src/main/java/first/webapp/HelloServlet.java
+++ src/main/java/first/webapp/HelloServlet.java
@@ -36,12 +36,13 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
 		
 		String yourName = request.getParameter("yourName");
 		PrintWriter writer = response.getWriter();
 		writer.println("<h1>Hello " + yourName + "</h1>");
 		writer.close();
+		
+		 
 	}
 
 }
